fdbeeb5b9004de8ffbc65bf3d23d97535dfcc3f1
moduhan <devduhanmo@gmail.com>
2022-03-03 23:57:25 +0900

    first-commit

---
+++ spring-data-jpa/jpashop-java/src/test/java/jpastudy/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package jpastudy.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional // 쓸 수 있는 옵션이 많다 spring version
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
