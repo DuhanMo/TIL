d5e1f3c3f1ef34812e9676b5721cee45495cb5a4
duhanmo <devduhanmo@gmail.com>
2022-03-29 18:18:47 +0900

    feat: Job Parameter를 통한 JobInstance 생성, Job에 대한 이해

--- kotlin-springbatch/src/main/kotlin/com/example/kotlinspringbatch/job/SimpleJobConfiguration.kt
+++ kotlin-springbatch/src/main/kotlin/com/example/kotlinspringbatch/job/SimpleJobConfiguration.kt
@@ -4,9 +4,11 @@ import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.util.logging.Logger
@@ -23,15 +25,30 @@ class SimpleJobConfiguration(
     @Bean
     fun simpleJob(): Job {
         return jobBuilderFactory["simpleJob"] //"simplejob" 이란 이름의 BatchJob 생성
-            .start(simpleStep1())
+            .start(simpleStep1(null))
+            .next(simpleStep2(null))
             .build()
     }
 
     @Bean
-    fun simpleStep1(): Step {
+    @JobScope
+    fun simpleStep1(@Value("#{jobParameters[requestDate]}") requestDate: String?): Step {
         return stepBuilderFactory["simpleStep1"]
             .tasklet { _: StepContribution, _: ChunkContext ->
                 logger.info(">>>>>>>>> This is Step1")
+                logger.info(">>>>>>>>> requestDate = $requestDate")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+
+    @Bean
+    @JobScope
+    fun simpleStep2(@Value("#{jobParameters[requestDate]}") requestDate: String?): Step {
+        return stepBuilderFactory["simpleStep2"]
+            .tasklet { _: StepContribution, _: ChunkContext ->
+                logger.info(">>>>>>>>> This is Step2")
+                logger.info(">>>>>>>>> requestDate = $requestDate")
                 RepeatStatus.FINISHED
             }
             .build()
